2153ab80d139a015a036cf91591b3a716d9f37fc
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 23:00:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(array)
   return array.none? {|item| item.length > 4}
 end
 
-puts long_planeteer_calls(["arra", "hi", "yow"])
+puts long_planeteer_calls(["axe", "earth", "wind", "fire"])
 
 def find_the_cheese# code an argument here
   # the array below is here to help
