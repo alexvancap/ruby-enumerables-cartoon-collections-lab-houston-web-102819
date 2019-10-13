adc5110ac6335c289a7c83e9109f73ef1b53fd46
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 22:51:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,8 @@ def long_planeteer_calls(array)
   }
 end
 
+long_planeteer_calls(["arra", "hi", "yow"])
+
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
