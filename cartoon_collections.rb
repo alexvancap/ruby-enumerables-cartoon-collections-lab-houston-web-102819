cf1288bb91382e4da65ff22bb0cbe5f18c3d22a1
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 23:07:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,14 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  counter = 0
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include? {|string|}
+  array.reduce{|string|
+    if array.
+  }
+  
+  }
 end
 
 find_the_cheese(["brie", "kees", "cheddar"])
