21a61acafce3787b56b22d25ba26575560e2878b
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 23:18:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,10 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  counter = 0
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.reduce{|string|
+    counter = 0
     while cheese_types[counter]
 
     end
