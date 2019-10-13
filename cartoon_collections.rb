f2723e65e6efe96660e293efa19738cc285d55db
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 23:18:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.reduce{|string|
-    while cheese_types[index]
+    while cheese_types[counter]
 
     end
   }
