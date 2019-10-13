4a75518fa9eb80e36b5cf794690a27e07b680041
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-13 23:27:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.reduce{|string|
-    return string if string.include?(string)
+    return string if string.include?(cheese_types)
   }
 end
 
